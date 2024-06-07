fc41c7426747f24277c353976476821d75e60b52
alvarixxs <alvarodesants@gmail.com>
2024-06-07 12:48:16 +0200

    finished phonebook exercises up to 2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,69 @@
 import {useEffect, useState} from 'react'
-import Filter from "./Filter.jsx";
-import NewName from "./NewName.jsx";
+import Filter from "./components/Filter.jsx";
+import NewName from "./components/NewName.jsx";
 import Numbers from "./components/Numbers.jsx";
-import axios from "axios";
+import personService from "./services/persons.js"
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [message, setMessage] = useState(null)
+  const [cl, setCl] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(res => {setPersons(res.data)})
+    personService
+      .getAll()
+      .then(res => {setPersons(
+        res.data)
+      })
   }, []);
 
   function addPerson(e) {
     e.preventDefault()
-    if (persons.find((person) => person.name === newName) !== undefined) {
-      alert(newName+ " is already added to phonebook")
+    let person = persons.find((person) => person.name === newName)
+    if (person !== undefined) {
+      if (window.confirm(person.name + " is already added to phonebook, replace the old number with a new one?")) {
+        personService
+          .update(person.id, {name: person.name, number: newNumber, id: person.id})
+          .then((res) => {
+            setPersons(persons.map((person)=> {
+              return {name: person.name, number: (person.id===res.data.id) ? res.data.number: person.number, id: person.id}
+            }))
+          })
+          .catch(() => {
+            setMessage("Information of " + person.name + " has already been removed from the server")
+            setCl('error')
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
+          })
+      }
     }
     else {
-      setPersons(persons.concat({name: newName, number: newNumber, id: persons.length+1}))
+      const person = {name: newName, number: newNumber}
+      personService
+        .create(person)
+        .then(res => {
+          setPersons(persons.concat(res.data))
+          setNewName('')
+          setNewNumber('')
+          setMessage("Added " + person.name)
+          setCl('success')
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
+    }
+  }
+
+  function deletePerson(person) {
+    if (window.confirm("Delete "+person.name + " ?")) {
+      personService
+        .remove(person.id)
+        .then(res => {
+          setPersons(persons.filter((person)=>person.id !== res.data.id))
+        })
     }
   }
 
@@ -27,9 +71,24 @@ const App = () => {
 
   return (
     <div>
+      <h2>Phonebook</h2>
+      <Notification message={message} cl={cl} />
       <Filter searchName={searchName} setSearchName={setSearchName}></Filter>
-      <NewName newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} addPerson={addPerson}></NewName>
-      <Numbers persons={persons} searchName={searchName}></Numbers>
+      <NewName newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber}
+               addPerson={addPerson}></NewName>
+      <Numbers persons={persons} searchName={searchName} remove={deletePerson}></Numbers>
+    </div>
+  )
+}
+
+const Notification = ({ message, cl }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={cl}>
+      {message}
     </div>
   )
 }
--- part2/phonebook/src/components/Numbers.jsx
+++ part2/phonebook/src/components/Numbers.jsx
@@ -1,8 +1,20 @@
-function Numbers({persons, searchName}) {
+function Numbers({persons, searchName, remove}) {
   return (
     <div>
       <h2>Numbers</h2>
-      {persons.filter((person)=>person.name.toLowerCase().includes(searchName.toLowerCase())).map((person) => <p key={person.id}>{person.name} {person.number}</p>)}
+      {
+        persons
+        .filter((person)=>person.name.toLowerCase().includes(searchName.toLowerCase()))
+        .map((person) => <Number key={person.id} person={person} remove={remove}></Number>)
+      }
+    </div>
+  )
+}
+
+function Number({person, remove}) {
+  return (
+    <div>
+      {person.name} {person.number} <button onClick={()=>remove(person)}>Delete</button>
     </div>
   )
 }
